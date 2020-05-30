c08169bfd762adff3281828b44f544196c4abf31
Ian Saucy <iansaucy@gmail.com>
2020-05-30 01:52:26 -0400

    The CSS is killing me

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,26 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader, getThemeVariables } = require('customize-cra');
+const { addReactRefresh } = require('customize-cra-react-refresh')
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: true
+    style: true,
   }),
   //https://github.com/ant-design/ant-design-landing/issues/235
   addLessLoader({
     lessOptions: {
       javascriptEnabled: true,
-      modifyVars: { '@primary-color': '#1DA57A' },
+      modifyVars: {
+        '@primary-color': '#ffca0f',
+        '@font-size-base': '16px',
+        '@text-color': '#292929',
+        '@layout-body-background': '#fffff',
+        '@btn-font-weight': '600',
+        
+
+      },
     },
   }),
-  
-  
+  addReactRefresh()
 );
