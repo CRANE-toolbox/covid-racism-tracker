c431e0ca9b1b3dd9e97d5ecc62245f9f8fb63a1b
LaChapeliere <emmamarion.barme@orange.fr>
2020-06-03 00:14:46 +0200

    Same topic gets same colour for all graphs

--- src/charts/areaBump.tsx
+++ src/charts/areaBump.tsx
@@ -12,10 +12,7 @@ interface props {
 
 }
 
-const colorSchemeRachael = ["#FFCA0F", "#ED5958", "#3CAEA3", "#B9D870", "#F89900", "#8DB039", "#63A5A7", "#8D88BD", "#BBD1D5"];
-const colorSchemeSveta1 = ["#ED5959", "#68C6A4", "#F1B531", "#20639B", "#61157C", "#FFCA0F", "#68C6A4", "#F6DEB9", "#F28665", "#DBDD15"]
-const colorSchemeSveta2 = ["#FFCA0F", "#8D88BD", "#BBD1D5", "#63A5A7", "#8DB039", "#F89900", "#B9D870", "#3CAEA3", "#424C6E", "#ED5958"]
-const colorSchemeSveta3 = ["#CCDB3A", "#673BB7", "#8BC24A", "#00BCD2", "#E71E62", "#FDC006", "#9C28B1", "#2196F3", "#FEEA3B", "#ED5958"]
+const finalColorScheme = ["#0B5DA2", "#138F60", "#48A4E3", "#8BC24A", "#9C28B1", "#BF6196", "#C8C5FD", "#DE8E08", "#E71E67"]
 
 const theme = {
     axis: {
@@ -57,7 +54,35 @@ export const CustomResponsiveBump = (props: props) => (
         }}
         theme={theme}
         spacing={8}
-        colors={ colorSchemeRachael }
+        colors={ ({ id }) => {
+            if (id === "Negative sentiment towards China/Chinese people" || id === "China-related, including alternative names for covid19" || id === "Linking China and Covid19") {
+              return(finalColorScheme[0])
+            }
+            else if (id === "Mistrust, cover-up, and lies" || id === "Opinion debates around medicine") {
+              return(finalColorScheme[1])
+            }
+            else if (id === "Location names") {
+              return(finalColorScheme[2])
+            }
+            else if (id === "Biowar") {
+              return(finalColorScheme[3])
+            }
+            else if (id === "Alternative names for Covid19" || id === "Neutral alternative names for Covid19") {
+              return(finalColorScheme[4])
+            }
+            else if (id === "International trade and diplomacy") {
+              return(finalColorScheme[5])
+            }
+            else if (id === "USA politics" || id === "Related to countries other than China") {
+              return(finalColorScheme[6])
+            }
+            else if (id === "Chinese politics") {
+              return(finalColorScheme[7])
+            }
+            else {
+              return(finalColorScheme[8])
+            }
+        }}
         blendMode="multiply"
         axisTop={{
             tickSize: 5,
