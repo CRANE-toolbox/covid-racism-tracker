3d1e71eac7b7f2a8f0438b7b79d6cabf4bbf9d80
Kelly <lykelly2014@icloud.com>
2020-06-03 18:01:50 -0400

    partners page

--- src/pages/SupportPage.tsx
+++ src/pages/SupportPage.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Row, Col, Layout, Button } from 'antd';
+import styles from '../styles/HomePage.module.less';
+import { useHistory } from 'react-router-dom';
+import { Routes } from '../routes';
 interface Props {}
 export const SupportPage: React.FC<Props> = () => {
   return <div>Support</div>;
