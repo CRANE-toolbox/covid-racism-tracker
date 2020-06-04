4feebace73bfef7667014ab01d7ab9b601ca5190
Ian Saucy <iansaucy@gmail.com>
2020-06-04 01:22:22 -0400

    Questionable logo placement

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Row, Col } from 'antd';
 import styles from '../styles/NavHeader.module.less';
 import { useHistory } from 'react-router-dom';
 import { Routes } from '../routes';
+import { ReactComponent as Logo } from '../assets/LOGO.svg';
 interface Props {}
 
 //Menu bar with buttons
@@ -10,53 +11,60 @@ export const Header: React.FC<Props> = () => {
   let history = useHistory();
   return (
     <Layout.Header className={styles.MenuHeader}>
-      <Menu className={styles.MenuBar} mode="horizontal" defaultSelectedKeys={['1']}>
-        <Menu.Item
-          key="1"
-          className={styles.MenuItem}
-          onClick={() => {
-            history.push(Routes.HOME);
-          }}
-        >
-          Home
-        </Menu.Item>
-        <Menu.Item
-          key="2"
-          className={styles.MenuItem}
-          onClick={() => {
-            history.push(Routes.ABOUT);
-          }}
-        >
-          About
-        </Menu.Item>
-        <Menu.Item
-          key="3"
-          className={styles.MenuItem}
-          onClick={() => {
-            history.push(Routes.DATA);
-          }}
-        >
-          Data
-        </Menu.Item>
-        <Menu.Item
-          key="4"
-          className={styles.MenuItem}
-          onClick={() => {
-            history.push(Routes.SUPPORT);
-          }}
-        >
-          Support
-        </Menu.Item>
-        <Menu.Item
-          key="5"
-          className={styles.MenuItem}
-          onClick={() => {
-            history.push(Routes.PARTNERS);
-          }}
-        >
-          Partners
-        </Menu.Item>
-      </Menu>
+      <Row align="top">
+        <Col>
+          <Logo className={styles.Logo} onClick={() => history.push(Routes.HOME)} />
+        </Col>
+        <Col className={styles.MenuCol}>
+          <Menu className={styles.MenuBar} mode="horizontal" defaultSelectedKeys={['1']}>
+            <Menu.Item
+              key="1"
+              className={styles.MenuItem}
+              onClick={() => {
+                history.push(Routes.HOME);
+              }}
+            >
+              Home
+            </Menu.Item>
+            <Menu.Item
+              key="2"
+              className={styles.MenuItem}
+              onClick={() => {
+                history.push(Routes.ABOUT);
+              }}
+            >
+              About
+            </Menu.Item>
+            <Menu.Item
+              key="3"
+              className={styles.MenuItem}
+              onClick={() => {
+                history.push(Routes.DATA);
+              }}
+            >
+              Data
+            </Menu.Item>
+            <Menu.Item
+              key="4"
+              className={styles.MenuItem}
+              onClick={() => {
+                history.push(Routes.SUPPORT);
+              }}
+            >
+              Support
+            </Menu.Item>
+            <Menu.Item
+              key="5"
+              className={styles.MenuItem}
+              onClick={() => {
+                history.push(Routes.PARTNERS);
+              }}
+            >
+              Partners
+            </Menu.Item>
+          </Menu>
+        </Col>
+      </Row>
     </Layout.Header>
   );
 };
