e1a601f61ac414451cefa8674b6bd096dd52d1a5
Kelly <lykelly2014@icloud.com>
2020-06-05 00:42:00 -0400

    hopefully this merges properly

---
+++ src/assets/assets.index.ts
@@ -0,0 +1,38 @@
+import React from 'react';
+
+/**
+ * Profile Photos
+ */
+export { default as rachaelProfile } from './profilePhotos/rachael_profile.jpg';
+export { default as cameliaProfile } from './profilePhotos/camelia_profile.jpg';
+export { default as ianProfile } from './profilePhotos/ian_profile.jpg';
+export { default as gianlucaProfile } from './profilePhotos/gianluca_profile.png';
+
+/**
+ * Social Icons
+ */
+
+export { ReactComponent as DevPost } from './social/devpost.svg';
+export { ReactComponent as Github } from './social/github.svg';
+export { ReactComponent as CircleEmail } from './social/circle_email.svg';
+
+/**
+ * Logos and other branding
+ */
+
+export { ReactComponent as Logo } from './LOGO.svg';
+
+/**
+ * Component assets
+ * Todo: Maybe move these inline to the components?
+ */
+
+export { ReactComponent as FlowGraphImg } from './components/flow-graph.svg';
+export { ReactComponent as LineGraphImg } from './components/graph-line.svg';
+export { ReactComponent as CircleClose } from './components/circle-close.svg';
+
+/**
+ * Random
+ */
+
+export { ReactComponent as SocialDistanceImg } from './social_distancing.svg';
