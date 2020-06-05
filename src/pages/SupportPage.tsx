bf4d8971447e38aacd51102646ddf51c853712ca
Kelly <lykelly2014@icloud.com>
2020-06-05 12:59:42 -0400

    support styling added

--- src/pages/SupportPage.tsx
+++ src/pages/SupportPage.tsx
@@ -9,7 +9,7 @@ interface Props {}
 export const SupportPage: React.FC<Props> = () => {
   let history = useHistory();
   return (
-    <div className={styles.PageWrapper}>
+    <div>
       <div className={styles.SupportContainer}>
         <Row>
           <Col span={24} className={styles.Title}>
@@ -27,50 +27,51 @@ export const SupportPage: React.FC<Props> = () => {
         </Row>
       </div>
 
-      <div className={styles.SupportContainer}>
-        <Row justify="center">
-          <Col span={24} className={styles.Title}>
-            <h1>Who can help me?</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={24}>
-            <body className={styles.SubTitleContent}>
-              <p>
-                If you have been a target of hate speech, racism, and/or xenophobia online or in-person following the Coronavirus pandemic, you are not alone. Many are being
-                harassed in response to COVID-19 based off their ethnicity and race. Below are a list of resouces, including hotlines, virtual support systems, and outreach groups.
-              </p>
-              <p>
-                If you are in immediate danger, <strong>please call 911</strong>.
-              </p>
-            </body>
-          </Col>
-        </Row>
-        <Row justify="center" className={styles.ActionButtonsRow}>
-          <Col>
-            <Button
-              size="large"
-              autoFocus={true}
-              className={styles.ActionButton}
-              onClick={() => {
-                history.push(Routes.DATA.path);
-              }}
-            >
-              Call 911 now
-            </Button>
-          </Col>
-          <Col>
-            <Button
-              size="large"
-              className={styles.ActionButton}
-              onClick={() => {
-                history.push(Routes.SUPPORT.path);
-              }}
-            >
-              Get Support
-            </Button>
-          </Col>
-        </Row>
+      <div className={styles.HelpSection}>
+        <div className={styles.SupportContainer}>
+          <Row justify="center">
+            <Col span={24} className={styles.Title}>
+              <h1 className={styles.HelpSubtitle}>Who can help me?</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <div className={styles.HelpContent}>
+                <p>
+                  If you have been a target of hate speech, racism, and/or xenophobia online or in-person following the Coronavirus pandemic, you are not alone. Many are being
+                  harassed in response to COVID-19 based off their ethnicity and race. Below are a list of resouces, including hotlines, virtual support systems, and outreach groups.
+                </p>
+                <p>If you are in immediate danger, <strong>please call 911</strong>.</p>
+              </div>
+            </Col>
+          </Row>
+
+          <Row justify="center" className={styles.ActionButtonsRow}>
+            <Col>
+              <Button
+                size="large"
+                autoFocus={true}
+                className={styles.ActionButtonLeft}
+                onClick={() => {
+                  history.push(Routes.DATA.path);
+                }}
+              >
+                Call 911 now
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                size="large"
+                className={styles.ActionButtonRight}
+                onClick={() => {
+                  history.push(Routes.SUPPORT.path);
+                }}
+              >
+                Get Support
+              </Button>
+            </Col>
+          </Row>
+        </div>
       </div>
 
       <Row justify="center">
@@ -92,7 +93,7 @@ export const SupportPage: React.FC<Props> = () => {
         <Col>
           <ResourceCard
             Icon={CardIcon.ORB}
-            body="Campaign Zero is a digital activist group fighting against police brutality. Funds donated to Campaign Zero support the analysis of policing practices across the country, research to identify effective solutions to end police violence, technical assistance to organizers 
+            body="Campaign Zero is a digital activist group fighting against police brutality. Funds donated to Campaign Zero support the analysis of policing practices across the country, research to identify effective solutions to end police violence, technical assistance to organizers
             leading police accountability campaigns and the development of model legislation and advocacy to end police violence nationwide."
             title="CAMPAIGN ZERO"
             linkText="Learn More"
@@ -102,7 +103,7 @@ export const SupportPage: React.FC<Props> = () => {
         <Col>
           <ResourceCard
             Icon={CardIcon.PERSON}
-            body="This toolkit represents the work and thinking of 15 grassroots organizations with Asian American bases living in the most precarious margins of power. All of the modules are designed to begin with people’s 
+            body="This toolkit represents the work and thinking of 15 grassroots organizations with Asian American bases living in the most precarious margins of power. All of the modules are designed to begin with people’s
              lived experiences, and to build structural awareness of why those experiences are happening, and how they are tied to the oppression of others."
             title="ASIAN AMERICAN TOOLKIT"
             linkText="Learn More"
@@ -171,7 +172,7 @@ export const SupportPage: React.FC<Props> = () => {
         <Col>
           <ResourceCard
             Icon={CardIcon.DUELPERSON}
-            body="In response to the rise in Anti-Asian/American and xenophobic harassment, Hollaback! 
+            body="In response to the rise in Anti-Asian/American and xenophobic harassment, Hollaback!
             has partnered with Asian Americans Advancing Justice (AAJC) to adapt their free bystander intervention and de-escalation training."
             title="BYSTANDER INTERVENTION TRAINING"
             linkText="Learn More"
@@ -180,7 +181,7 @@ export const SupportPage: React.FC<Props> = () => {
         <Col>
           <ResourceCard
             Icon={CardIcon.PERSON}
-            body="We’re fighting fire with water, and working step by step to assist victims, educate communities, and stop harassment online. HeartMob aims to be the place where 
+            body="We’re fighting fire with water, and working step by step to assist victims, educate communities, and stop harassment online. HeartMob aims to be the place where
             those facing harassment can easily report abuse across social networks and find support from others who know what they're going through "
             title="HEART MOB"
             linkText="Learn More"
@@ -189,7 +190,7 @@ export const SupportPage: React.FC<Props> = () => {
         <Col>
           <ResourceCard
             Icon={CardIcon.PERSON}
-            body="Learn how to help survivors of hate crime. When we as a society become more aware of the rise in hate speech and the resources available, 
+            body="Learn how to help survivors of hate crime. When we as a society become more aware of the rise in hate speech and the resources available,
             we can help those being targeted. Stay informed! Safe Horizon outlines the dimensions of hate speech and what concerned supporters should know!"
             title="SAFE HORIZON"
             linkText="Learn More"
